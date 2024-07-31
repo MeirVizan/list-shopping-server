d2becbd9725a9229c302aa3b1827cfaa60032ece
MeirVizan <meir15102@gmail.com>
2024-07-31 23:20:54 +0300

    fixed data table

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import { Sequelize } from 'sequelize';
+import { Category, Product } from './models';
+var cors = require('cors')
+
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const sequelize = new Sequelize('sqlite::memory:');
+Category.initialize(sequelize);
+Product.initialize(sequelize);
+
+sequelize.sync({ force: true }).then(() => {
+  const categories = ['Cleaning Products', 'Cheeses', 'Vegetables and Fruits', 'Meat and Fish', 'Pastries'];
+  categories.forEach(async (category) => {
+    await Category.create({ name: category });
+  });
+});
+
+app.get('/api/categories', async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    res.json(categories.map((c) => ({'id': c.id , 'name':c.name})));
+  } catch (error) {
+    res.status(500).send("Failed to fetch categories.");
+  }
+});
+
+app.post('/api/order', async (req, res) => {
+  try{
+    const products = req.body.products;
+    await Product.bulkCreate(products);
+    res.status(201).send('Order saved');
+  }
+  catch (e) {
+    res.status(500).send("Failed to save order.");
+  }
+});
+
+app.get('/api/purchases', async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  }
+  catch (e) {
+    res.status(500).send("Failed to fetch purchases.");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/models.ts
@@ -0,0 +1,69 @@
+import { Model, DataTypes, Sequelize } from 'sequelize';
+
+export class Category extends Model {
+  public id!: number;
+  public name!: string;
+
+  public static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: new DataTypes.STRING(128),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: 'Category',
+      }
+    );
+  }
+}
+
+export class Product extends Model {
+  public id!: number;
+  public name!: string;
+  public categoryId!: number;
+  public quantity!: number;
+
+  public static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: new DataTypes.STRING(128),
+          allowNull: false,
+        },
+        categoryId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: Category,
+            key: 'id',
+          },
+        },
+        // category: {
+        //   type: new DataTypes.STRING(128),
+        //   allowNull: false,
+        // },
+        quantity: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: 'Product',
+      }
+    );
+  }
+}
